781536a524cdbe253bd843ff9ae37818c214ffd9
Ibrahim Al Tuwayhir <bfh33355@gmail.com>
2026-08-13 14:27:21 +0300

    Add validation queries for customer, OTP log, and account

---
+++ SQL/ValidationQueries.sql
@@ -0,0 +1,5 @@
+SELECT * FROM CUSTOMER WHERE NATIONAL_ID='1023456789';
+________________________________________________
+SELECT * FROM OTP_LOG WHERE MOBILE='0501234567';
+___________________________________________________
+SELECT * FROM ACCOUNT WHERE ACCOUNT_STATUS='ACTIVE';
